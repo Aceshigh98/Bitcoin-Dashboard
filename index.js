14cc401d03e57243ba290d7c2774725b9f56f431
AcesHigh <cothermanbrannon@gmail.com>
2024-02-13 20:47:02 -0600

    02/13/24

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.get("/btcdata", async (req, res) => {
 });
 
 // Start the server
-app.listen(8080, () => {
-  console.log("Server is running on port 8080");
+app.listen(443, () => {
+  console.log("Server is running on port 443");
 });
 
