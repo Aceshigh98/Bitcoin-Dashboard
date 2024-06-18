d0a3b47aecc56b6f68cdbc6062f0ac86f5991824
AcesHigh <cothermanbrannon@gmail.com>
2024-06-18 14:24:51 -0500

    6/18/2024

--- client/public/src/utils/API.js
+++ client/public/src/utils/API.js
@@ -7,7 +7,7 @@ import { renderCirculationChart } from "../scripts/circulationChart.js";
 import { renderProgressBar } from "../scripts/halving.js";
 
 const getBTCData = () => {
-  return fetch("http://api.aceshighbitcoin.com:5000/data") // Replace with your server URL and endpoint
+  return fetch("http://www.aceshighbitcoin.com/data") // Replace with your server URL and endpoint
     .then((response) => {
       if (!response.ok) {
         throw new Error("Network response was not ok");
